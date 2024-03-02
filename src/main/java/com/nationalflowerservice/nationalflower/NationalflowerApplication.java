081ddeec2eea486163e48e3319b7e0d759949353
yoshi-koyama <koyama@reytech.co.jp>
2024-03-02 21:30:54 +0900

    プロジェクトのセットアップ

---
+++ src/main/java/com/nationalflowerservice/nationalflower/NationalflowerApplication.java
@@ -0,0 +1,13 @@
+package com.nationalflowerservice.nationalflower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NationalflowerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NationalflowerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nationalflowerservice/nationalflower/NationalflowerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nationalflowerservice.nationalflower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NationalflowerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
